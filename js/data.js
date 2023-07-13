1704a1957b9708e00ce7ef1af57b5e3cfee00dcc
alireza zahedi <alireza@g,aim.com>
2023-07-13 21:34:26 +0330

    add file

---
+++ js/data.js
@@ -0,0 +1,162 @@
+let allData = {
+  products: [
+    {
+      id: 1,
+      name: "Multi Midnight",
+      price: 190.0,
+      about:
+        "Posuere in netus a eu varius adipiscing suspendisse elementum vitae tempor suspendisse ullamcorper aenean taciti morbi potenti cursus id tortor. Cursus nulla consectetur a eros adipiscing himenaeos nam taciti id turpis a scelerisque vel habitasse.",
+      sizeWatch: [32, 42, 48],
+      category: "watch",
+      pictureSrc: "img/products/product-watch-3.jpg",
+      slider: {
+        slidePictureSrc: [
+          "img/products/watch-slide-2-6.png",
+          "img/products/watch-slide-2-7.png",
+          "img/products/watch-slide-2-8.png",
+        ],
+        h2: "Swiss-made stylish brand blue watches.",
+        p: "Lorem ipsum dolor sit amet, consectetur adipisicing elit. Accusamus porro similique labore praesentium.",
+        bgPrimery: "rgb(42, 41, 41)",
+        bgsecondery:
+          "linear-gradient(90deg, rgba(247, 247, 247, 1) 50%, rgb(42, 41, 41) 50%)",
+      },
+    },
+    {
+      id: 2,
+      name: "Classic Neu Black",
+      price: 160.0,
+      about:
+        "Posuere in netus a eu varius adipiscing suspendisse elementum vitae tempor suspendisse ullamcorper aenean taciti morbi potenti cursus id tortor. Cursus nulla consectetur a eros adipiscing himenaeos nam taciti id turpis a scelerisque vel habitasse.",
+      sizeWatch: [32, 42, 48],
+      category: "watch",
+      pictureSrc: "img/products/product-watch-2.jpg",
+    },
+    {
+      id: 3,
+      name: "Multi Morning",
+      price: 165.0,
+      about:
+        "Posuere in netus a eu varius adipiscing suspendisse elementum vitae tempor suspendisse ullamcorper aenean taciti morbi potenti cursus id tortor. Cursus nulla consectetur a eros adipiscing himenaeos nam taciti id turpis a scelerisque vel habitasse.",
+      sizeWatch: [32, 42, 48],
+      category: "watch",
+      pictureSrc: "img/products/product-watch-4.jpg",
+    },
+    {
+      id: 4,
+      name: "Multi Noon",
+      price: 135.0,
+      about:
+        "Posuere in netus a eu varius adipiscing suspendisse elementum vitae tempor suspendisse ullamcorper aenean taciti morbi potenti cursus id tortor. Cursus nulla consectetur a eros adipiscing himenaeos nam taciti id turpis a scelerisque vel habitasse.",
+      sizeWatch: [32, 42, 48],
+      category: "watch",
+      pictureSrc: "img/products/product-watch-5.jpg",
+    },
+    {
+      id: 5,
+      name: "Multi Midday",
+      price: 140.0,
+      about:
+        "Posuere in netus a eu varius adipiscing suspendisse elementum vitae tempor suspendisse ullamcorper aenean taciti morbi potenti cursus id tortor. Cursus nulla consectetur a eros adipiscing himenaeos nam taciti id turpis a scelerisque vel habitasse.",
+      sizeWatch: [32, 42, 48],
+      category: "watch",
+      pictureSrc: "img/products/product-watch-3_edited.jpg",
+      slider: {
+        slidePictureSrc: [
+          "img/products/watch-slide-1-2-1.png",
+          "img/products/watch-slide-1-7.png",
+          "img/products/watch-slide-1-6.png",
+        ],
+        h2: "Swiss-made stylish brand blue watches.",
+        p: "Lorem ipsum dolor sit amet, consectetur adipisicing elit. Accusamus porro similique labore praesentium.",
+        bgPrimery: "#9fc0d3",
+        bgsecondery:
+          "linear-gradient(90deg, rgba(247, 247, 247, 1) 50%, #9fc0d3 50%)",
+      },
+    },
+  ],
+  magazine: [
+    {
+      id: 1,
+      title: "Collar brings back coffee brewing ritual",
+      group: ["DESIGN TRENDS"],
+      imgSrc: "img/magazine/watch-post-4.jpg",
+      datePublished:1680250226619,
+      writer: {
+        pictureSrc: "img/magazine/226d936abb812698ad55af91b0545dad.jpeg",
+        name: "Rogers",
+      },
+      comment: [],
+      shortIntroduce:
+        "Aliquet parturient scele risque scele risque nib pretium parturient suspendisse platea sapie torquent feugiat parturient hac amet. Volutpa  nul...",
+    },
+    {
+      id: 2,
+      title: "Exterior ideas: 10 colored garden seats",
+      group: ["DESIGN TRENDS"],
+      imgSrc: "img/magazine/watch-post-3.jpg",
+      datePublished:1671015026619,
+      writer: {
+        pictureSrc: "img/magazine/226d936abb812698ad55af91b0545dad.jpeg",
+        name: "Rogers",
+      },
+      comment: [],
+      shortIntroduce:
+        "A sed a risusat luctus esta anibh rhoncus hendrerit blandit nam rutrum sitmiad hac. Crass a vestibulum a varius adipiscing ut dignissim ullamcorp...",
+    },
+    {
+      id: 3,
+      title: "The big design: Wall likes pictures",
+      group: ["DESIGN TRENDS"],
+      imgSrc: "img/magazine/watch-post-2.jpg",
+      datePublished:1658823026619,
+      writer: {
+        pictureSrc: "img/magazine/226d936abb812698ad55af91b0545dad.jpeg",
+        name: "Rogers",
+      },
+      comment: [],
+      shortIntroduce:
+        "Parturient in potenti id rutrum duis torquent parturient sceler isque sit vestibulum a posuerescelerisque viverra urna. Egestas tristique vestib...",
+    },
+  ],
+  instagramPost: [
+    {
+      id: 1,
+      imgSrc: 'img/instagram/wd-inst-watch-1.jpg.webp',
+      like: 200,
+      comment: 15,
+    },
+    {
+      id: 2,
+      imgSrc: 'img/instagram/wd-inst-watch-2.jpg.webp',
+      like: 455,
+      comment: 455,
+    },
+    {
+      id: 3,
+      imgSrc: 'img/instagram/wd-inst-watch-3.jpg.webp',
+      like: 2000,
+      comment: 457,
+    },
+    {
+      id: 4,
+      imgSrc: 'img/instagram/wd-inst-watch-4.jpg.webp',
+      like: 250,
+      comment: 78,
+    },
+    {
+      id: 5,
+      imgSrc: 'img/instagram/wd-inst-watch-5.jpg.webp',
+      like: 571,
+      comment: 668,
+    },
+    {
+      id: 6,
+      imgSrc: "img/instagram/wd-inst-watch-6-430x430.jpg.webp",
+      like: 55,
+      comment: 45,
+    },
+  ],
+};
+
+export { allData };
